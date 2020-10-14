a2b869fa9c5facbbc91af3a24e0020232bde89e9
Javier Moreno <mq2880@gmail.com>
2020-10-14 13:04:42 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DevolverLetras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           letra("Z");
+
+        }
+         
+        static void letra(string letra)
+        {
+            string linea = "";
+
+            for (int row = 0; row < 7 ;row++)
+            {
+                for (int column = 0; column < 7 ; column++)
+                {
+                    //letra V
+                    if(letra == "V")
+                    {
+                    linea += (column == 0 || column == 6) && (row == 0 || row == 1) ? "*": 
+                    (column == 1 || column == 5) && (row == 2 || row == 3) ? "*": 
+                    (column == 2 || column == 4) && (row == 4 || row == 5) ? "*": 
+                    (column == 3) && (row == 6) ? "*": " ";
+                    }
+
+                    if(letra == "Z")
+                    {
+                    //letra Z
+                    linea += (column >= 0 || column <= 6) && (row == 0 || row == 6) ? "*": 
+                    (column == 1 && row == 5) ? "*": (column == 2 && row == 4) ? "*": 
+                    (column == 3 && row == 3) ? "*": (column == 4 && row == 2) ? "*": 
+                    (column == 5 && row == 1) ? "*":  " ";
+                    }
+                }
+                Console.WriteLine(linea);
+                linea = "";
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Al método se le pasa la letra que se quiere copiar, en este caso solo pinta la V y la Z
+
+![image-20201014125634002](C:\Users\mq288\AppData\Roaming\Typora\typora-user-images\image-20201014125634002.png)
